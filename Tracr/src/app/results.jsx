c73510fc9d4c3b56691668b9d49f5e96c7be585c
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-03-11 18:20:39 +0000

    create results page with two comparison images and 3 buttons

---
+++ Tracr/src/app/results.jsx
@@ -0,0 +1,355 @@
+import { StyleSheet, View, Pressable, Image, Modal, ImageBackground } from "react-native";
+import { useNavigation } from "expo-router";
+import { ThemedText } from "@/src/components/themed-text";
+import { useState } from "react";
+
+export default function ResultsScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const navigation = useNavigation();
+
+  const handleSave = () => {
+    navigation.navigate("stats")
+  }
+
+  const handleRetry = () => {
+    navigation.navigate("map")
+  }
+
+  const handleShare = () => {
+    setModalVisible(true)
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={{ uri: "https://img.freepik.com/premium-vector/children-drawings-seamless-pattern-kids-doodle-texture-hand-drawn-cute-house-cat-frog-unicorn-baby-seamless-pattern-editable-stroke-vector-illustration-white-background_192280-1324.jpg" }}
+        resizeMode="cover"
+        style={styles.backgroundImage}
+      />
+      <Image
+        source={{ uri: "https://images.squarespace-cdn.com/content/v1/5b4dbfd8da02bcfcf39bce03/1710251915806-Z7CK432GWPNKMPPG9OG1/heart4-2022-02-14-at-11.10.03.jpg" }}
+        style={styles.userRoute} />
+
+      <Image source={require("@/assets/images/red-outline-heart2.jpg")}
+        style={styles.targetRoute} />
+
+      <ThemedText>73% MATCH</ThemedText>
+      <ThemedText>2000 POINTS</ThemedText>
+
+
+
+      <Pressable
+        onPress={() => handleSave()}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed,
+          styles.saveButton,
+        ]}>
+        <ThemedText>Save</ThemedText>
+      </Pressable>
+
+
+      <Pressable
+        onPress={() => handleRetry()}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed,
+          styles.retryButton,
+        ]}>
+        <ThemedText>Retry</ThemedText>
+      </Pressable>
+
+
+      <Pressable
+        onPress={() => handleShare()}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed,
+          styles.shareButton,
+        ]}>
+        <ThemedText>Share</ThemedText>
+      </Pressable>
+
+      <Modal visible={modalVisible} transparent={true}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalBox}>
+            <ThemedText>[insert icons]</ThemedText>
+            <Pressable
+              onPress={() => setModalVisible(false)}>
+              <ThemedText>Cancel</ThemedText>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  backgroundImage: {
+    ...StyleSheet.absoluteFillObject,
+    opacity: 0.1,
+  },
+
+  userRoute: {
+    width: 100,
+    height: 100,
+  },
+
+  targetRoute: {
+    width: 100,
+    height: 100,
+  },
+
+  saveButton: {
+    backgroundColor: "#84f984",
+    textAlign: "center",
+  },
+
+  retryButton: {
+    backgroundColor: "#cdcecd",
+    textAlign: "center",
+  },
+
+  shareButton: {
+    backgroundColor: "#5f98f3",
+    textAlign: "center",
+  },
+
+  modalBackground: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  modalBox: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 12,
+    alignItems: "center",
+    width: 220,
+  },
+
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 23,
+    marginBottom: 10,
+    fontWeight: "900",
+    fontSize: 18,
+    borderWidth: 3,
+    borderColor: "black",
+    borderRadius: 7,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 0,
+    elevation: 2,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    color: "black",
+  },
+
+  buttonPressed: {
+    transform: [{ translateX: 1 }, { translateY: 1 }],
+    shadowOffset: { width: 1, height: 1 },
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// import { StyleSheet, View, Pressable, Text, Animated } from "react-native";
+// import { ThemedText } from "@/src/components/themed-text";
+// import { useNavigation } from "expo-router";
+// import { useRef, useEffect } from "react";
+
+// export default function ResultsScreen() {
+
+//   const navigation = useNavigation();
+
+
+//   const leftX = useRef(new Animated.Value(-150)).current;
+// const rightX = useRef(new Animated.Value(150)).current;
+
+// useEffect(() => {
+//   Animated.parallel([
+//     Animated.timing(leftX, {
+//       toValue: 0,
+//       duration: 2000,
+//       useNativeDriver: true,
+//     }),
+//     Animated.timing(rightX, {
+//       toValue: 0,
+//       duration: 1000,
+//       useNativeDriver: true,
+//     }),
+//   ]).start();
+// }, []);
+
+
+//   const handleSave = () => {
+//     navigation.navigate("stats")
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       {/* <View>
+//         <Image 
+//         source={{ uri: "https://images.squarespace-cdn.com/content/v1/5b4dbfd8da02bcfcf39bce03/1710251915806-Z7CK432GWPNKMPPG9OG1/heart4-2022-02-14-at-11.10.03.jpg" }} 
+//         style={styles.userRoute}/>
+
+//         <Image source={require("@/assets/images/red-outline-heart2.jpg")} 
+//         style={styles.targetRoute}/>
+//       </View> */}
+
+// <View style={styles.overlayContainer}>
+
+//   <Animated.Image
+//     source={require("@/assets/images/red-outline-heart2.jpg")}
+//     style={[
+//       styles.targetRoute,
+//       { transform: [{ translateX: leftX }] }
+//     ]}
+//   />
+
+//   <Animated.Image
+//     source={{ uri: "https://images.squarespace-cdn.com/content/v1/5b4dbfd8da02bcfcf39bce03/1710251915806-Z7CK432GWPNKMPPG9OG1/heart4-2022-02-14-at-11.10.03.jpg" }}
+//     style={[
+//       styles.userRoute,
+//       { transform: [{ translateX: rightX }] }
+//     ]}
+//   />
+
+// </View>
+
+//       <Text>73% MATCH</Text>
+//       <Text>2000 POINTS</Text>
+
+
+//       <Pressable
+//         onPress={() => handleSave()}
+//         style={({ pressed }) => [
+//           styles.button,
+//           pressed && styles.buttonPressed,
+//           styles.saveButton,
+//         ]}>
+//         <Text>Save</Text>
+//       </Pressable>
+
+
+//       <Pressable
+//         onPress={() => setModalVisible(false)}
+//         style={({ pressed }) => [
+//           styles.button,
+//           pressed && styles.buttonPressed,
+//           styles.retryButton,
+//         ]}>
+//         <Text>Retry</Text>
+//       </Pressable>
+
+
+//       <Pressable
+//         onPress={() => setModalVisible(false)}
+//         style={({ pressed }) => [
+//           styles.button,
+//           pressed && styles.buttonPressed,
+//           styles.shareButton,
+//         ]}>
+//         <Text>Share</Text>
+//       </Pressable>
+
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+
+//   userRoute: {
+//     width: 100,
+//     height: 100,
+//   },
+
+//   targetRoute: {
+//     width: 100,
+//     height: 100,
+//   },
+
+//   overlayContainer: {
+//   width: 200,
+//   height: 200,
+//   justifyContent: "center",
+//   alignItems: "center",
+//   marginBottom: 30,
+// },
+
+//   routeImage: {
+//   position: "absolute",
+//   width: 200,
+//   height: 200,
+//   resizeMode: "contain",
+// },
+
+//   saveButton: {
+//     backgroundColor: "#84f984",
+//     textAlign: "center",
+//   },
+
+//   retryButton: {
+//     backgroundColor: "#cdcecd",
+//     textAlign: "center",
+//   },
+
+//   shareButton: {
+//     backgroundColor: "#5f98f3",
+//     textAlign: "center",
+//   },
+
+//   button: {
+//     paddingVertical: 10, // 0.6em ≈ 10px (based on 18px font size)
+//     paddingHorizontal: 23, // 1.3em ≈ 23px
+//     marginBottom: 10,
+//     fontWeight: "900",
+//     fontSize: 18,
+//     borderWidth: 3,
+//     borderColor: "black",
+//     borderRadius: 7, // 0.4em ≈ 7px
+//     shadowColor: "black",
+//     shadowOffset: { width: 2, height: 2 }, // 0.1em ≈ 2px
+//     shadowOpacity: 1,
+//     shadowRadius: 0,
+//     elevation: 2, // Android shadow
+//     textAlign: "center",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     alignContent: "center",
+//     color: "black",
+//   },
+//   buttonPressed: {
+//     transform: [{ translateX: 1 }, { translateY: 1 }],
+//     shadowOffset: { width: 1, height: 1 },
+//   },
+// });
