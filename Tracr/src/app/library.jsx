876d7a527f98f273991f485189dadcec74ed678f
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-03-16 19:57:09 +0000

    change navigation to expo router and create vector path for target shapes

--- Tracr/src/app/library.jsx
+++ Tracr/src/app/library.jsx
@@ -8,63 +8,142 @@ import {
   Image,
   ImageBackground,
 } from "react-native";
-import { useNavigation } from "expo-router";
+import { router } from "expo-router"
 import { useState } from "react";
 
 export default function Shapes() {
   const [selectedShape, setSelectedShape] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
-  const navigation = useNavigation();
 
   const shapes = [
     {
       id: 1,
-      name: "Star",
+      name: "star",
       image_url:
         "https://images.emojiterra.com/google/android-10/512px/2b50.png",
+      path: [
+      { x: 0.5, y: 0.05 },
+      { x: 0.6, y: 0.35 },
+      { x: 0.95, y: 0.35 },
+      { x: 0.65, y: 0.55 },
+      { x: 0.75, y: 0.9 },
+      { x: 0.5, y: 0.7 },
+      { x: 0.25, y: 0.9 },
+      { x: 0.35, y: 0.55 },
+      { x: 0.05, y: 0.35 },
+      { x: 0.4, y: 0.35 },
+      { x: 0.5, y: 0.05 }
+    ]
     },
     {
       id: 2,
-      name: "Heart",
+      name: "heart",
       image_url:
         "https://images.emojiterra.com/google/android-10/512px/2764.png",
+        path: [
+  { x: 0.50, y: 0.85 },
+  { x: 0.38, y: 0.78 },
+  { x: 0.27, y: 0.70 },
+  { x: 0.20, y: 0.60 },
+  { x: 0.18, y: 0.49 },
+  { x: 0.19, y: 0.39 },
+  { x: 0.24, y: 0.31 },
+  { x: 0.31, y: 0.27 },
+  { x: 0.39, y: 0.26 },
+  { x: 0.445, y: 0.275 },
+  { x: 0.47, y: 0.315 },
+  { x: 0.50, y: 0.38 },
+  { x: 0.53, y: 0.315 },
+  { x: 0.555, y: 0.275 },
+  { x: 0.61, y: 0.26 },
+  { x: 0.69, y: 0.27 },
+  { x: 0.76, y: 0.31 },
+  { x: 0.81, y: 0.39 },
+  { x: 0.82, y: 0.49 },
+  { x: 0.80, y: 0.60 },
+  { x: 0.73, y: 0.70 },
+  { x: 0.62, y: 0.78 },
+  { x: 0.50, y: 0.85 },
+]
     },
     {
-      id: 3,
-      name: "Fire",
-      image_url:
-        "https://images.emojiterra.com/google/android-11/512px/1f525.png",
-    },
-    {
-      id: 4,
-      name: "Flag",
-      image_url:
-        "https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f6a9.png",
-    },
-    {
-      id: 5,
-      name: "Shoe",
-      image_url:
-        "https://images.emojiterra.com/google/android-12l/512px/1f45f.png",
-    },
-    {
-      id: 6,
-      name: "Dog",
-      image_url:
-        "https://images.emojiterra.com/google/android-11/512px/1f436.png",
-    },
-    {
-      id: 7,
-      name: "Turtle",
-      image_url:
-        "https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/1024px/1f422.png",
-    },
-    {
-      id: 8,
-      name: "Merperson",
-      image_url:
-        "https://images.emojiterra.com/microsoft/fluent-emoji/15.1/3d/1f9dc_3d.png",
-    },
+  id: 3,
+  name: "Arrow",
+  image_url: "https://png.pngtree.com/element_our/sm/20180308/sm_5aa0fdd386c8a.png",
+  path: [
+    { x: 0.2, y: 0.5 },
+    { x: 0.6, y: 0.5 },
+    { x: 0.6, y: 0.3 },
+    { x: 0.9, y: 0.6 },
+    { x: 0.6, y: 0.9 },
+    { x: 0.6, y: 0.7 },
+    { x: 0.2, y: 0.7 },
+    { x: 0.2, y: 0.5 }
+  ]
+},
+{
+  id: 4,
+  name: "Lightning Bolt",
+  image_url: "https://images.emojiterra.com/google/android-11/256px/26a1.png",
+  path: [
+    { x: 0.5, y: 0.1 },
+    { x: 0.3, y: 0.5 },
+    { x: 0.55, y: 0.5 },
+    { x: 0.35, y: 0.9 },
+    { x: 0.7, y: 0.4 },
+    { x: 0.45, y: 0.4 },
+    { x: 0.5, y: 0.1 }
+  ]
+},
+{
+  id: 5,
+  name: "Gem",
+  image_url: "https://em-content.zobj.net/thumbs/160/apple/354/gem-stone_1f48e.png",
+  path: [
+  { x: 0.15, y: 0.40 },
+  { x: 0.35, y: 0.20 },
+  { x: 0.65, y: 0.20 },
+  { x: 0.85, y: 0.40 },
+  { x: 0.50, y: 0.85 },
+  { x: 0.15, y: 0.40 }
+]
+}
+    // {
+    //   id: 3,
+    //   name: "Fire",
+    //   image_url:
+    //     "https://images.emojiterra.com/google/android-11/512px/1f525.png",
+    // },
+    // {
+    //   id: 4,
+    //   name: "Flag",
+    //   image_url:
+    //     "https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f6a9.png",
+    // },
+    // {
+    //   id: 5,
+    //   name: "Shoe",
+    //   image_url:
+    //     "https://images.emojiterra.com/google/android-12l/512px/1f45f.png",
+    // },
+    // {
+    //   id: 6,
+    //   name: "Dog",
+    //   image_url:
+    //     "https://images.emojiterra.com/google/android-11/512px/1f436.png",
+    // },
+    // {
+    //   id: 7,
+    //   name: "Turtle",
+    //   image_url:
+    //     "https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/1024px/1f422.png",
+    // },
+    // {
+    //   id: 8,
+    //   name: "Merperson",
+    //   image_url:
+    //     "https://images.emojiterra.com/microsoft/fluent-emoji/15.1/3d/1f9dc_3d.png",
+    // },
   ];
 
   const handleSelection = (shape) => {
@@ -72,9 +151,14 @@ export default function Shapes() {
     setModalVisible(true);
   };
 
-  const handleConfirm = (shape) => {
+  const handleConfirm = () => {
     setModalVisible(false);
-    navigation.navigate("map");
+    router.push({
+      pathname: "/map",
+      params: {
+        shape: JSON.stringify(selectedShape),
+      },
+    });
   };
 
   const bgImage1 =
@@ -111,7 +195,7 @@ export default function Shapes() {
             <Text style={styles.modalText}>Start this run?</Text>
 
             <Pressable
-              onPress={() => handleConfirm(selectedShape)}
+              onPress={() => handleConfirm()}
               style={({ pressed }) => [
                 styles.button,
                 pressed && styles.buttonPressed,
