c4e13b77f97b18f0adc1120af33483e78ce6bd00
Abigail Lee <226848368+abigailys@users.noreply.github.com>
2026-03-17 08:50:40 +0000

    create data file to move target shapes data from library to data file

---
+++ Tracr/data/shapes.js
@@ -0,0 +1,92 @@
+export const shapes = [
+    {
+      id: 1,
+      name: "star",
+      image_url:
+        "https://images.emojiterra.com/google/android-10/512px/2b50.png",
+      path: [
+      { x: 0.5, y: 0.05 },
+      { x: 0.6, y: 0.35 },
+      { x: 0.95, y: 0.35 },
+      { x: 0.65, y: 0.55 },
+      { x: 0.75, y: 0.9 },
+      { x: 0.5, y: 0.7 },
+      { x: 0.25, y: 0.9 },
+      { x: 0.35, y: 0.55 },
+      { x: 0.05, y: 0.35 },
+      { x: 0.4, y: 0.35 },
+      { x: 0.5, y: 0.05 }
+    ]
+    },
+    {
+      id: 2,
+      name: "heart",
+      image_url:
+        "https://images.emojiterra.com/google/android-10/512px/2764.png",
+        path: [
+  { x: 0.50, y: 0.85 },
+  { x: 0.38, y: 0.78 },
+  { x: 0.27, y: 0.70 },
+  { x: 0.20, y: 0.60 },
+  { x: 0.18, y: 0.49 },
+  { x: 0.19, y: 0.39 },
+  { x: 0.24, y: 0.31 },
+  { x: 0.31, y: 0.27 },
+  { x: 0.39, y: 0.26 },
+  { x: 0.445, y: 0.275 },
+  { x: 0.47, y: 0.315 },
+  { x: 0.50, y: 0.38 },
+  { x: 0.53, y: 0.315 },
+  { x: 0.555, y: 0.275 },
+  { x: 0.61, y: 0.26 },
+  { x: 0.69, y: 0.27 },
+  { x: 0.76, y: 0.31 },
+  { x: 0.81, y: 0.39 },
+  { x: 0.82, y: 0.49 },
+  { x: 0.80, y: 0.60 },
+  { x: 0.73, y: 0.70 },
+  { x: 0.62, y: 0.78 },
+  { x: 0.50, y: 0.85 },
+]
+    },
+    {
+  id: 3,
+  name: "Arrow",
+  image_url: "https://png.pngtree.com/element_our/sm/20180308/sm_5aa0fdd386c8a.png",
+  path: [
+    { x: 0.2, y: 0.5 },
+    { x: 0.6, y: 0.5 },
+    { x: 0.6, y: 0.3 },
+    { x: 0.9, y: 0.6 },
+    { x: 0.6, y: 0.9 },
+    { x: 0.6, y: 0.7 },
+    { x: 0.2, y: 0.7 },
+    { x: 0.2, y: 0.5 }
+  ]
+},
+{
+  id: 4,
+  name: "Lightning Bolt",
+  image_url: "https://images.emojiterra.com/google/android-11/256px/26a1.png",
+  path: [
+    { x: 0.5, y: 0.1 },
+    { x: 0.3, y: 0.5 },
+    { x: 0.55, y: 0.5 },
+    { x: 0.35, y: 0.9 },
+    { x: 0.7, y: 0.4 },
+    { x: 0.45, y: 0.4 },
+    { x: 0.5, y: 0.1 }
+  ]
+},
+{
+  id: 5,
+  name: "Gem",
+  image_url: "https://em-content.zobj.net/thumbs/160/apple/354/gem-stone_1f48e.png",
+  path: [
+  { x: 0.15, y: 0.40 },
+  { x: 0.35, y: 0.20 },
+  { x: 0.65, y: 0.20 },
+  { x: 0.85, y: 0.40 },
+  { x: 0.50, y: 0.85 },
+  { x: 0.15, y: 0.40 }
+]}]
