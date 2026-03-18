258d36d8e044fa74ded01bbecdd5075897b6ffa6
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-03-18 17:22:54 +0000

    updated styling. Set dynamic labels and values which reflect the selected metric and timeframe

--- Tracr/src/components/StatsChart.jsx
+++ Tracr/src/components/StatsChart.jsx
@@ -1,54 +1,103 @@
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { LineChart } from "react-native-gifted-charts";
 
-export default function StatsChart() {
+export default function StatsChart({ data, dateRange, timeframe, metric }) {
   const [spaces, setSpaces] = useState(7);
-  const [period, setPeriod] = useState("day");
-  const data = [
-    { value: 15 },
-    { value: 30 },
-    { value: 26 },
-    { value: 40 },
-    { value: 40 },
-    { value: 40 },
-    { value: 40 },
+  const [dataSet, setDataSet] = useState(null);
+
+  const week = ["mon", "tue", "wed", "thur", "fri", "sat", "sun"];
+  const month = [
+    "1st",
+    "2nd",
+    "3rd",
+    "4th",
+    "5th",
+    "6th",
+    "7th",
+    "8th",
+    "9th",
+    "10th",
+    "11th",
+    "12th",
+    "13th",
+    "14th",
+    "15th",
+    "16th",
+    "17th",
+    "18th",
+    "19th",
+    "20th",
+    "21st",
+    "22nd",
+    "23rd",
+    "24th",
+    "25th",
+    "26th",
+    "27th",
+    "28th",
+    "29th",
+    "30th",
+    "31st",
+  ];
+  const year = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sept",
+    "oct",
+    "nov",
+    "dec",
   ];
 
-  // useEffect(() => setSpaces(7), []);
-
-  // switch (period) {
-  //   case "day":
-  //     setSpaces(7);
-  //     break;
-  //   case "month":
-  //     setSpaces(10);
-  //     break;
-  //   case "year":
-  //     setSpaces(12);
-  //     break;
-  //   case "all time":
-  //     setSpaces(24);
-  // }
+  console.log("I am data in statschart>>>", data);
+  useEffect(() => {
+    if (data === null) return;
+    const dataPoints = data.map((dataPoint, i) => {
+      if (timeframe === "week") {
+        setSpaces(7);
+        return { value: dataPoint, label: week[i] };
+      } else if (timeframe === "month") {
+        setSpaces(30);
+        return { value: dataPoint, label: month[i] };
+      } else if (timeframe === "year") {
+        setSpaces(12);
+        return { value: dataPoint, label: year[i] };
+      }
+      return { value: dataPoint, label: i + 1 };
+    });
+    setDataSet(dataPoints);
+  }, [data]);
 
   return (
     <View style={styles.container}>
-      <LineChart
-        style={styles.lineChart}
-        data={data}
-        // hideDataPoints
-        // hideRules
-        // hideYAxisText
-        curved
-        isAnimated
-        color="#04e704"
-        thickness={5}
-        initialSpacing={0}
-        endSpacing={0}
-        spacing={200 / spaces - 1}
-        width={200}
-        // verticalLinesSpacing={spaces}
-      />
+      <Text style={styles.stats}>Stats</Text>
+      <Text>{dateRange}</Text>
+      {dataSet && (
+        <LineChart
+          style={styles.lineChart}
+          data={dataSet}
+          maxValue={metric === "accuracy" ? 100 : undefined}
+          hideDataPoints
+          yAxisThickness={0}
+          xAxisLabelsAtBottom
+          rotateLabel
+          curved
+          isAnimated
+          color="#5cbdfd"
+          thickness={5}
+          adjustToWidth
+          initialSpacing={1}
+          spacing={timeframe === "month" ? 1000 / spaces : 500 / spaces + 1}
+          noOfSections={5}
+          width={250}
+        />
+      )}
     </View>
   );
 }
@@ -64,9 +113,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 0,
     elevation: 2, // Android shadow
-    marginTop: 20,
+    // marginBottom: 20,
     paddingTop: 20,
+    paddingBottom: 20,
     paddingLeft: 20,
-    width: "100%",
+  },
+  stats: {
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: "ui-monospace",
   },
 });
