5da6798bc64f7c9b047304a359f54aee0d7a1462
Dragan Ajvazovic <204484118+Dragan-London@users.noreply.github.com>
2026-03-19 11:23:10 +0000

    added padding to the top so it centers the page

--- Tracr/src/app/login.jsx
+++ Tracr/src/app/login.jsx
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
-    paddingTop: 0,
+    paddingTop: 36,
   },
   logo: {
     marginBottom: 40,
